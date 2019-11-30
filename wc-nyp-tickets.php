ab668cfdedd0d43ccdf5bb3533df708ca5bef6c4
Kathy Daring <helgatheviking@gmail.com>
2019-11-30 11:35:22 -0700

    environment checking

--- wc-nyp-tickets.php
+++ wc-nyp-tickets.php
@@ -10,10 +10,10 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: wc-nyp-tickets
  * Domain Path: /languages
- * Requires at least: 4.0.0
- * Tested up to: 4.8.1
- * WC requires at least: 3.0.0
- * WC tested up to: 3.2.0   
+ * Requires at least: 5.0.0
+ * Tested up to: 5.3.0
+ * WC requires at least: 3.6.0
+ * WC tested up to: 3.8.0   
  */
 
 /**
@@ -31,7 +31,9 @@ class WC_NYP_Tickets {
 
 	const VERSION = '1.1.0';
 	const PREFIX  = 'WC_NYP_Tickets';
-	const REQUIRED_WC = '3.0.0';
+	const REQUIRED_WC = '3.6.0';
+	const REQUIRED_NYP = '2.10.0';
+	const REQUIRED_TICKETS = '4.10.0';
 
 	/**
 	 * @var WC_NYP_Tickets - the single instance of the class
@@ -92,14 +94,80 @@ class WC_NYP_Tickets {
 
 	public function __construct(){
 
-		// Load core files.
-		add_action( 'plugins_loaded', array( $this, 'required_files' ), 20 );
-
 		// Load translation files.
 		add_action( 'init', array( $this, 'load_plugin_textdomain' ), 20 );
 
+		// Sanity checks.
+		if( ! $this->has_min_environment() ) {
+			add_action( 'admin_notices', array( $this, 'admin_notices' ) );
+			return false;
+		}
+
+		// Load core files.
+		add_action( 'plugins_loaded', array( $this, 'required_files' ), 20 );
+
 	}
 
+
+	/**
+	 * Test environement meets min requirements.
+	 *
+	 * @since  1.1.0
+	 */
+	public function has_min_environment() {
+
+		$has_min_environment = true;
+		$notices = array();
+
+		// WC version sanity check.
+		if ( ! defined( 'WC_VERSION' ) || version_compare( WC_VERSION, self::REQUIRED_WC, '<' ) ) {
+			$notice = sprintf( __( '<strong>Name Your Price Tickets is inactive.</strong> The %sWooCommerce plugin%s must be active and at least version %s for Name Your Price Tickets to function. Please upgrade or activate WooCommerce.', 'wc-nyp-tickets' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">', '</a>', self::REQUIRED_WC );
+
+			$notices[] = $notice;
+			$has_min_environment = false;
+		}
+
+		// Name Your Price version sanity check.
+		if ( ! function_exists( 'WC_Name_Your_Price' ) || version_compare( WC_Name_Your_Price()->version, self::REQUIRED_NYP, '<' ) ) {
+			$notice = sprintf( __( '<strong>Name Your Price Tickets is inactive.</strong> The %sWooCommerce Name Your Price plugin%s must be active and at least version %s for Name Your Price Tickets to function. Please upgrade or activate WooCommerce Name Your Price.', 'wc-nyp-tickets' ), '<a href="http://woocommerce.com/products/name-your-price/">', '</a>', self::REQUIRED_NYP );
+
+			$notices[] = $notice;
+			$has_min_environment = false;
+		}
+
+		// Event Tickets version sanity check.
+		if ( ! class_exists( 'Tribe__Tickets_Plus__Main' ) || version_compare( Tribe__Tickets_Plus__Main::VERSION, self::REQUIRED_TICKETS, '<' ) ) {
+			$notice = sprintf( __( '<strong>Name Your Price Tickets is inactive.</strong> The %sEvents Tickets Plus plugin%s must be active and at least version %s for Name Your Price Tickets to function. Please upgrade or activate WooCommerce.', 'wc-nyp-tickets' ), '<a href="https://theeventscalendar.com/product/wordpress-event-tickets-plus/">', '</a>', self::REQUIRED_TICKETS );
+
+			$notices[] = $notice;
+			$has_min_environment = false;
+		}
+
+		if( ! empty( $notices ) ) { error_log(json_encode( $notices));
+			update_option( 'wc_nyp_tickets_notices', $notices );
+		}
+		return $has_min_environment;
+
+	}
+
+
+	/**
+	 * Displays a warning message if version check fails.
+	 *
+	 * @return string
+	 */
+	public function admin_notices() {
+
+		$notices = get_option( 'wc_nyp_tickets_notices', array() );
+
+		if( ! empty( $notices ) ) {
+			foreach( $notices as $notice ) {
+				 echo '<div class="error"><p>' . $notice . '</p></div>';
+			}
+			delete_option( 'wc_nyp_tickets_notices' );
+		}
+	   
+	}
 
 	/*-----------------------------------------------------------------------------------*/
 	/* Required Files */
@@ -212,4 +280,4 @@ function WC_NYP_Tickets() {
 }
 
 // Launch the whole plugin
-add_action( 'wc_name_your_price_loaded', 'WC_NYP_Tickets' );
+add_action( 'plugins_loaded', 'WC_NYP_Tickets' );
