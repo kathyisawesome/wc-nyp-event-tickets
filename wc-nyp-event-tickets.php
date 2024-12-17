831cacd3502ac6ece52d7c129a19e04858fa15e8
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-12-17 12:30:54 -0500

    version bump 2.1.0

--- wc-nyp-event-tickets.php
+++ wc-nyp-event-tickets.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Name Your Price - Event Tickets
  * Plugin URI:  https://github.com/kathyisawesome/wc-nyp-event-tickets
  * Description: Bridge plugin for adding NYP support to Modern Tribe&#39;s Tickets Plus
- * Version: 2.0.3
+ * Version: 2.1.0
  * Author:      Kathy Darling
  * Author URI:  http://www.kathyisawesome.com
  * License: GNU General Public License v3.0
@@ -46,7 +46,7 @@ if ( ! class_exists( 'WC_NYP_Tickets' ) ) :
 
 class WC_NYP_Tickets {
 
-	const VERSION = '2.0.3';
+	const VERSION = '2.1.0';
 	const PREFIX  = 'WC_NYP_Tickets';
 	const REQUIRED_WC = '9.3.0';
 	const REQUIRED_NYP = '3.0.0';
