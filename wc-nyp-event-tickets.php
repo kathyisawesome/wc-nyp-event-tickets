f665e566f330887c4dbe0349987a911bf82e1e42
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2025-01-03 12:28:58 -0500

    Add required PHP 7.4 header

--- wc-nyp-event-tickets.php
+++ wc-nyp-event-tickets.php
@@ -11,6 +11,7 @@
  * Text Domain: wc-nyp-event-tickets
  * Domain Path: /languages
  * Requires at least: 6.6.0
+ * Requires PHP: 7.4
  * Tested up to: 6.7.0
  * WC requires at least: 9.3.0
  * WC tested up to: 9.5.0  
